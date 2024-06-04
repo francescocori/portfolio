b960a90a11cfaae4c91fdf95ad31b86fdc3d4981
Francesco <francesco.coriolano@gmail.com>
2024-06-04 21:53:27 +0100

    organise folders

---
+++ src/components/organisms/AboutMe/AboutMe.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AboutMe = () => {
+  return <div className="">aboutMe</div>;
+};
---
+++ src/components/organisms/index.ts
@@ -0,0 +1,4 @@
+export * from "./AboutMe/AboutMe";
+export * from "./Hero/Hero";
+export * from "./Projects/Projects";
+export * from "./Skills/Skills";
