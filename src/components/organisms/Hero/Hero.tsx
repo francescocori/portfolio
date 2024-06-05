58e9414ead1e94f6ee725bea463b6cb957a36b30
Francesco <francesco.coriolano@gmail.com>
2024-06-05 14:48:04 +0100

    added colors to hero page and style

--- src/components/organisms/Hero/Hero.tsx
+++ src/components/organisms/Hero/Hero.tsx
@@ -1,22 +1,29 @@
-import React from "react";
+import * as React from "react";
+//maybe delete this
+import useMediaQuery from "@mui/material/useMediaQuery";
 import "./style.css";
+
 export const Hero = () => {
   return (
     <div className="hero flex h-screen w-full items-center px-20 xxl:px-40">
-      <div className="w-[57%]">
+      <div className="sm:w-[100%] xl:w-[57%]">
         <h1 className="text-[52px] font-bold text-white xxl:text-[60px]">
           Hello! <br /> I'm{" "}
-          <span className="underline underline-offset-[10px]">
-            Francesco Coriolano.
-          </span>
+          <div className="nom text-transparent from-indigo-500 via-sky-500 to-emerald-500 relative inline-block bg-gradient-to-r from-10% via-30% to-90% bg-clip-text pt-0 underline">
+            <span>Francesco</span>
+            <span> Coriolano.</span>
+
+            <div className="from-indigo-500 via-sky-500 to-emerald-500 h-[4px] max-w-[full] bg-gradient-to-r from-10% via-30% to-90%" />
+          </div>
         </h1>
-        <p className="mt-10 text-[20px] leading-8 text-white">
+
+        <p className="mt-10 w-[100%] text-white sm:text-[18px] md:text-[20px] xl:leading-8">
           I'm a Italian Frontend developer based in Lisbon. I'm passionate about
           pixel-perfect beautiful interfaces and I love to add a magic touch to
           the internet.
         </p>
       </div>
-      <div className="flex hidden h-full w-[43%] items-center justify-center">
+      <div className="hidden h-full w-[43%] items-center justify-center sm:block">
         <div className="triangles">
           <div className="un"></div>
           <div className="deux"></div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,30 +8,30 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      // backgroundImage: {
-      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      //   "gradient-conic":
-      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      // },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        gradientStart: "rgba(100, 247, 217, 1)",
+        gradientMid: "rgba(100, 247, 217, 1)",
+        gradientEnd: "rgba(175, 162, 255, 1)",
+      },
     },
-    screens: {
-      sm: "320px",
-      md: "740px",
-      lg: "976px",
-      xl: "1200px",
-      xxl: "1441px",
-      "3xl": "1620px",
-      "4xl": "1920px",
-      "sm-tall": { raw: "(max-width: 740px) and (min-height: 751px)" },
-      small: { raw: "(min-width: 976px) and (max-height: 750px)" },
-      tall: { raw: "(min-width: 976px) and (min-height: 751px)" },
-      extra: { raw: "(min-width: 976px) and (min-height: 930px)" },
-    },
-
-    colors: {
-      white: "#FFFFFF",
-      orange: "#FF5F00",
-    },
+  },
+  screens: {
+    sm: "320px",
+    md: "740px",
+    lg: "976px",
+    xl: "1200px",
+    xxl: "1441px",
+    "3xl": "1620px",
+    "4xl": "1920px",
+    "sm-tall": { raw: "(max-width: 740px) and (min-height: 751px)" },
+    small: { raw: "(min-width: 976px) and (max-height: 750px)" },
+    tall: { raw: "(min-width: 976px) and (min-height: 751px)" },
+    extra: { raw: "(min-width: 976px) and (min-height: 930px)" },
   },
   plugins: [],
 };
