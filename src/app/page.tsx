a6b44f303d6c13c682318b50060e117da1633dd8
Francesco <francesco.coriolano@gmail.com>
2024-06-05 16:46:08 +0100

    created navbar wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { AboutMe, Hero, Projects, Skills } from "@/components/organisms";
+import { Nav } from "@/components/molecules";
 export default function Home() {
   return (
-    <main className="flex ">
+    <main className="flex">
       <div className="w-full">
+        <Nav />
         <Hero />
         {/* <AboutMe />
         <Skills />
--- src/components/molecules/Nav/Nav.tsx
+++ src/components/molecules/Nav/Nav.tsx
@@ -1,5 +1,67 @@
-import React from "react";
-
+"use client";
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 export const Nav = () => {
-  return <div>Nav</div>;
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
+
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  // Array containing navigation items
+  const navItems = [
+    { id: 1, text: "Home" },
+    { id: 2, text: "Company" },
+    { id: 3, text: "Resources" },
+    { id: 4, text: "About" },
+    { id: 5, text: "Contact" },
+  ];
+
+  return (
+    <div className="mx-auto flex h-24 max-w-[1240px] items-center justify-between bg-[#0c0a3d] px-4 text-white">
+      {/* Logo */}
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]"></h1>
+
+      {/* Desktop Navigation */}
+      <ul className="hidden md:flex">
+        {navItems.map((item) => (
+          <li
+            key={item.id}
+            className="hover:text-black m-2 cursor-pointer rounded-xl p-4 duration-300 hover:bg-[#00df9a]"
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile Navigation Icon */}
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+
+      {/* Mobile Navigation Menu */}
+      <ul
+        className={
+          nav
+            ? "border-r-gray-900 fixed left-0 top-0 h-full w-[60%] border-r bg-[#000300] duration-500 ease-in-out md:hidden"
+            : "fixed bottom-0 left-[-100%] top-0 w-[60%] duration-500 ease-in-out"
+        }
+      >
+        {/* Mobile Logo */}
+        <h1 className="m-4 w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+
+        {/* Mobile Navigation Items */}
+        {navItems.map((item) => (
+          <li
+            key={item.id}
+            className="hover:text-black border-gray-600 cursor-pointer rounded-xl border-b p-4 duration-300 hover:bg-[#00df9a]"
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
