654a7852834208db952d57fc922543eb5b6cc191
Francesco <francesco.coriolano@gmail.com>
2024-06-06 12:27:32 +0100

    created card for  aboutme section

--- src/components/organisms/AboutMe/AboutMe.tsx
+++ src/components/organisms/AboutMe/AboutMe.tsx
@@ -7,11 +7,16 @@ export const AboutMe = () => {
     Aos.init({ duration: 2000 });
   }, []);
   return (
+    // TODO:
+    // create a separate component for the card
+    // create an array for the data and pass the PROPs to the card
+    // improve the style code: ES: ML- AUTO  apply only on even childs
+    // make a line on thee middleof the cards
     <div className="flex h-screen w-full flex-col items-center">
       <h2 className="mt-[200px] text-[40px] font-bold text-[#0c0a3d]">
         About me
       </h2>
-      <p className="mx-40 pt-10 text-[22px] text-[#0c0a3d]">
+      <p className="mx-20 pt-10 text-[22px] text-[#0c0a3d]">
         I build websites and web applications using React and other
         technologies, such as Next Js, Typescript, and HTML/CSS/SCSS. I work
         with agile development methodologies and collaborate with other
@@ -20,7 +25,68 @@ export const AboutMe = () => {
         technologies and skills, and I believe that being a continuous learner
         is essential for every developer.
       </p>
-      <div className="bg-gray-600 h-[20rem] w-[20rem]" data-aos="fade-up"></div>
+      <div className="mt-10 h-screen w-full flex-col px-20">
+        <div
+          className="w-[500px] rounded-2xl bg-[#0c0a3d] p-[30px]"
+          data-aos="fade-right"
+        >
+          <p className="text-bold text-[22px] text-white">May 2022- today</p>
+          <div>
+            <p className="my-4 text-[18px] text-white">
+              Frontend Develoert - Studio Graphene
+            </p>
+            <span className="text-[#64f7d9]">
+              SCSS - JS - Vue - GTM - Figma - Cypress - Sitecore - Gulp -
+              Performance - Design - C#
+            </span>
+          </div>
+        </div>
+        <div
+          className="ml-auto w-[500px] rounded-2xl bg-[#0c0a3d] p-[30px]"
+          data-aos="fade-left"
+        >
+          <p className="text-bold text-[22px] text-white">May 2022- today</p>
+          <div>
+            <p className="my-4 text-[18px] text-white">
+              Frontend Develoert - Studio Graphene
+            </p>
+            <span className="text-[#64f7d9]">
+              SCSS - JS - Vue - GTM - Figma - Cypress - Sitecore - Gulp -
+              Performance - Design - C#
+            </span>
+          </div>
+        </div>
+        <div
+          className="w-[500px] rounded-2xl bg-[#0c0a3d] p-[30px]"
+          data-aos="fade-right"
+        >
+          <p className="text-bold text-[22px] text-white">May 2022- today</p>
+          <div>
+            <p className="my-4 text-[18px] text-white">
+              Frontend Develoert - Studio Graphene
+            </p>
+            <span className="text-[#64f7d9]">
+              SCSS - JS - Vue - GTM - Figma - Cypress - Sitecore - Gulp -
+              Performance - Design - C#
+            </span>
+          </div>
+        </div>
+        <div
+          className="ml-auto w-[500px] rounded-2xl bg-[#0c0a3d] p-[30px]"
+          data-aos="fade-left"
+        >
+          <p className="text-bold text-[22px] text-white">May 2022- today</p>
+          <div>
+            <p className="my-4 text-[18px] text-white">
+              Frontend Develoert - Studio Graphene
+            </p>
+            <span className="text-[#64f7d9]">
+              SCSS - JS - Vue - GTM - Figma - Cypress - Sitecore - Gulp -
+              Performance - Design - C#
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
