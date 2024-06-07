a4ee62b72ab254d3ce72b17451a73b1f4586f59b
Francesco <francesco.coriolano@gmail.com>
2024-06-07 16:33:41 +0100

    added skills icons

--- src/components/atoms/SkillCard/SkillCard.tsx
+++ src/components/atoms/SkillCard/SkillCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const SkillCard = () => {
-  return <div>SkillCard</div>;
+  return <div className="bg-blue-500 p-4 xl:h-[130px]">Item 1</div>;
 };
--- src/components/organisms/Skills/Skills.tsx
+++ src/components/organisms/Skills/Skills.tsx
@@ -2,8 +2,22 @@ import React from "react";
 
 export const Skills = () => {
   return (
-    <div className="h-screen w-full bg-[#0c0a3d]">
-      <h2>Skills</h2>
+    <div className="flex h-screen w-full flex-col items-center rounded-tl-[200px] bg-[#0c0a3d] px-20 py-[100px]">
+      <h2 className="mb-10 text-[40px] font-bold text-white">Skills</h2>
+      <div className="container mx-auto p-4">
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          <div className="bg-blue-500 p-4 xl:h-[130px]">Item 1</div>
+          <div className="bg-blue-500 p-4 xl:h-[130px]">Item 2</div>
+          <div className="bg-blue-500 p-4 xl:h-[130px]">Item 3</div>
+          <div className="bg-blue-500 p-4 xl:h-[130px]">Item 4</div>
+          <div className="bg-blue-500 p-4 xl:h-[130px]">Item 5</div>
+          <div className="bg-blue-500 p-4 xl:h-[130px]">Item 6</div>
+          <div className="bg-blue-500 p-4 xl:h-[130px]">Item 7</div>
+          <div className="bg-blue-500 p-4 xl:h-[130px]">Item 8</div>
+          <div className="bg-blue-500 p-4 xl:h-[130px]">Item 9</div>
+          <div className="bg-blue-500 p-4 xl:h-[130px]">Item 10</div>
+        </div>
+      </div>
     </div>
   );
 };
